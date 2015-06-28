65f13db0055e7194bd6ff080befe551d52a00ee0
Albert Tseng <albert@chegg.com>
2015-06-28 10:46:55 -0700

    style market gain and chart labels

--- scripts/investments.js
+++ scripts/investments.js
@@ -21,29 +21,57 @@ window.Investments = {
 
 	},
 	template: _.template('<div>' +
-							'<table style="width:100%;">' +
+							'<div class="market-value"><span class="market-value__sign">$</span>22,000<span class="market-value__cents">.00</span></div>' +
+							'<div class="day-gain">-2,000 (-10%) <span class="day-gain__span">TODAY</span></div>' +
+
+							'<table id="chart-list">' + 
+								'<tr>' + 
+									'<th>1 DAY</th>' +
+									'<th>5 DAYS</th>' +
+									'<th>1 MONTH</th>' +
+									'<th>3 MONTHS</th>' +
+									'<th>1 YEAR</th>' +
+									'<th>5 YEARS</th>' +
+									'<th>ALL</th>' +
+								'</tr>' + 
+							'</table>' +
+							'<table id="watchlist">' +
 								'<tr>' +
 									'<th>Symbol</th>' +
-									'<th>Shares</th>' +
 									'<th>Price</th>' +
-									'<th>Cost</th>' +
+									'<th>% Change</th>' +
 									'<th>Market Value</th>' +
+									'<th>Cost</th>' +
 									'<th>P/L</th>' +
 								'</tr>' +
 								'<% _.each(quotes, function(quote) { %>' +
 									'<tr>' +
-										'<td><%= quote.symbol.toUpperCase() %></td>' + 
-										'<td><%= portfolio[quote.symbol.toUpperCase()].shares %></td>' + 
-										'<td><%= quote.LastTradePriceOnly %></td>' + 
-										'<td>$<%= portfolio[quote.symbol.toUpperCase()].cost %></td>' + 
-										'<td>$<%= portfolio[quote.symbol.toUpperCase()].shares * quote.LastTradePriceOnly %></td>' + 
-										'<td><%= portfolio[quote.symbol.toUpperCase()].shares * quote.LastTradePriceOnly - portfolio[quote.symbol.toUpperCase()].cost %></td>' + 
+										'<td class="symbol"><%= quote.symbol.toUpperCase() %><br><span class="num-shares"><%= portfolio[quote.symbol.toUpperCase()].shares %> SHARES</span></td>' + 
+										'<td>$<%= quote.LastTradePriceOnly %></td>' + 
+										'<td><%= quote.PercentChange %></td>' + 
+										'<td>$<%= portfolio[quote.symbol.toUpperCase()].shares * parseInt(quote.LastTradePriceOnly).toFixed(2) %></td>' + 
+										'<td>$<%= parseInt(portfolio[quote.symbol.toUpperCase()].cost).toFixed(2) %></td>' + 
+										'<td>$<%= portfolio[quote.symbol.toUpperCase()].shares * parseInt(quote.LastTradePriceOnly).toFixed(2) - parseInt(portfolio[quote.symbol.toUpperCase()].cost).toFixed(2) %></td>' + 
 									'</tr>' +
 								'<% }) %>' +
 							'</table>' +
 						 '</div>'),
 	init: function() {
+		this.setBackgroundColor();
 		this.fetchQuotes();
+	},
+	setBackgroundColor: function() {
+		var date = new Date(Date.now());
+		var hour = date.getHours().toString();
+		var minutes = date.getMinutes().toString();
+		var timeOfDay = hour.concat(minutes);
+		if(timeOfDay > '1300' || timeOfDay < '630') {
+			$('article').css('background-color', '#020A11');
+			console.log('market close');
+		} else {
+			$('article').css('background-color', '#FFF');
+			console.log('market open');
+		}
 	},
 	fetchQuotes: function() {
 		var url = this.yahooYQL + this.quoteURL;
