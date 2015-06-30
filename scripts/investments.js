2b3c4fb738a7c7a654fcd465fde55d55edb0a542
Albert Tseng <albert@chegg.com>
2015-06-30 16:56:45 -0700

    try to get intraday prices

--- scripts/investments.js
+++ scripts/investments.js
@@ -1,8 +1,21 @@
 'use strict';
 
+// "1d",
+// "1m",
+// "3m",
+// "6m",
+// "1y",
+// "2y",
+// "5y"
+
+// http://chartapi.finance.yahoo.com/instrument/1.0/TSLA/chartdata;type=quote;range=1m/json
+
 window.Investments = {
+	chartAPI: 'http://chartapi.finance.yahoo.com/instrument/1.0/',
+	chartQuote: '/chartdata;type=quote;range=',
 	yahooYQL: 'https://query.yahooapis.com/v1/public/yql?q=',
 	quoteURL: 'select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(',
+
 	historicalURL: 'select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22YHOO%22%20and%20startDate%20%3D%20%222009-09-11%22%20and%20endDate%20%3D%20%222010-03-10%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
 	performance: {
 		oneday: {
@@ -16,44 +29,44 @@ window.Investments = {
 	},
 	portfolio: {
 		TSLA: {
-			shares: 1000,
-			cost: 150000
-			// shares: 608,
-			// cost: 119287
+			// shares: 1000,
+			// cost: 150000
+			shares: 608,
+			cost: 119287
 		},
 		AMBA: {
-			shares: 2000,
-			cost: 150000
-			// shares: 1758,
-			// cost: 200000
+			// shares: 2000,
+			// cost: 150000
+			shares: 1758,
+			cost: 200000
 		},
 		UA: {
-			shares: 1000,
-			cost: 60000
-			// shares: 409,
-			// cost: 33508
+			// shares: 1000,
+			// cost: 60000
+			shares: 409,
+			cost: 33508
 		}
 	},
 	quotes: {
 
 	},
 	template: _.template('<div>' +
-							'<div class="market-value"><span class="market-value__sign">$</span><%= portfolioValue %><span class="market-value__cents">.00</span></div>' +
-							'<div class="day-gain"><%= dayPL %> (<%= dayPercent %>%) <span class="day-gain__span">TODAY</span></div>' +
+							'<div class="market-value"><span class="market-value__sign">$ </span><%= portfolioValue %></div>' +
+							'<div class="day-gain <%= dayPLClass %>"><%= dayPL %> (<%= dayPercent %>%) <span class="day-gain__span">TODAY</span></div>' +
 
 							'<table id="chart-list">' + 
-								'<tr>' + 
-									'<th>1 DAY</th>' +
-									'<th>5 DAYS</th>' +
-									'<th>1 MONTH</th>' +
-									'<th>3 MONTHS</th>' +
-									'<th>1 YEAR</th>' +
-									'<th>5 YEARS</th>' +
-									'<th>ALL</th>' +
+								'<tr class="<%= dayPLClass %>">' + 
+									'<th class="chart-range selected" data-range="1d">1 DAY</th>' +
+									'<th data-range="5d">5 DAYS</th>' +
+									'<th data-range="1m">1 MONTH</th>' +
+									'<th data-range="3m">3 MONTHS</th>' +
+									'<th data-range="1y">1 YEAR</th>' +
+									'<th data-range="5y">5 YEARS</th>' +
+									'<th data-range="all">ALL</th>' +
 								'</tr>' + 
 							'</table>' +
 							'<table id="watchlist">' +
-								'<tr>' +
+								'<tr class="labels">' +
 									'<th>Symbol</th>' +
 									'<th>Price</th>' +
 									'<th>% Change</th>' +
@@ -63,20 +76,19 @@ window.Investments = {
 									'<th>P/L</th>' +
 								'</tr>' +
 								'<% _.each(quotes, function(quote) { %>' +
-									'<tr>' +
+									'<tr class="<%= quote.Change >= 0 ? "positive" : "negative" %>">' +
 										'<td class="symbol"><%= quote.Symbol %><br><span class="num-shares"><%= quote.Shares %> SHARES</span></td>' + 
 										'<td>$<%= quote.LastTradePriceOnly %></td>' + 
 										'<td><%= quote.PercentChange %></td>' + 
 										'<td>$<%= quote.todayPL %></td>' + 
 										'<td>$<%= quote.MarketValue %></td>' + 
 										'<td>$<%= quote.Cost %></td>' + 
-										'<td>$<%= quote.totalPL %></td>' + 
+										'<td><%= quote.totalPL %></td>' + 
 									'</tr>' +
 								'<% }) %>' +
 							'</table>' +
 						 '</div>'),
 	init: function() {
-		this.setBackgroundColor();
 		this.fetchQuotes();
 	},
 
@@ -84,11 +96,16 @@ window.Investments = {
 		var date = new Date(Date.now());
 		var hour = date.getHours().toString();
 		var minutes = date.getMinutes().toString();
-		var timeOfDay = hour.concat(minutes);
-		if(timeOfDay > '1300' || timeOfDay < '0630') {
+		var timeOfDay = parseInt(hour.concat(minutes));
+		debugger
+		if(timeOfDay > 1300 || timeOfDay < 630) {
+			$('.market-value').css('color', '#FFF');
+			$('.symbol').css('color', '#FFF');
 			$('body').css('background-color', '#020A11');
 			console.log('market close');
 		} else {
+			$('.market-value').css('color', '#000');
+			$('.symbol').css('color', '#000');
 			$('body').css('background-color', '#FFF');
 			console.log('market open');
 		}
@@ -112,25 +129,26 @@ window.Investments = {
 	massageData: function(quotes) {
 		var sortedArr = [];
 		var portfolio = this.portfolio;
+		var that = this;
 		_.each(quotes, function(quote) {
 			var sorted = {};
 			// for table
 			sorted['Symbol'] = quote.symbol.toUpperCase();
 			sorted['Shares'] = portfolio[sorted['Symbol']].shares;
-			sorted['LastTradePriceOnly'] = parseInt(quote.LastTradePriceOnly).toFixed(2);
+			sorted['LastTradePriceOnly'] = quote.LastTradePriceOnly;
 			sorted['PercentChange'] = quote.PercentChange;
 			sorted['todayPL'] = (quote.Change / sorted['LastTradePriceOnly'] * quote.PreviousClose * sorted['Shares']).toFixed(2);
-			sorted['MarketValue'] = sorted['Shares'] * sorted['LastTradePriceOnly'];
+			sorted['MarketValue'] = parseFloat(sorted['Shares'] * sorted['LastTradePriceOnly']).toFixed(2);
 			sorted['Cost'] = parseInt(portfolio[sorted['Symbol']].cost).toFixed(2);
-			sorted['totalPL'] = sorted['MarketValue'] - sorted['Cost'];
+			sorted['totalPL'] = that.numberFormat(sorted['MarketValue'] - sorted['Cost'], 2, true);
 			// extra
-			sorted['Change'] = parseInt(quote.Change).toFixed(2);
+			sorted['Change'] = quote.Change;
 			sorted['PreviousClose'] = parseInt(quote.PreviousClose).toFixed(2);
 			sortedArr.push(sorted);
 		});
 		return sortedArr;
 	},
-	numberFormat: function(number, dec, dsep, tsep) {
+	numberFormat: function(number, dec, money, dsep, tsep) {
 		if(isNaN(number) || number == null) return '';
 
 		number = number.toFixed(~~dec);
@@ -138,7 +156,10 @@ window.Investments = {
 
 		var parts = number.split('.'),
 			fnums = parts[0],
-			decimals = parts[1] ? (dsep || '.') + parts[1] : '';
+			decimals = parts[1] ? (dsep || '.') + parts[1] : '',
+			money = money ? '$' : '';
+		fnums = fnums.replace(/-/, '-&');
+		fnums = fnums.split('&').join('$');
 
 		return fnums.replace(/(\d)(?=(?:\d{3})+$)/g, '$1' + tsep) + decimals;
 	},
@@ -146,17 +167,30 @@ window.Investments = {
 		var portfolioValue = 0;
 		var dayPL = 0;
 		var dayPercent = 0;
+		var dayPLClass = 'positive';
 		_.each(this.quotes, function(quote) {
-			portfolioValue += parseInt(quote.MarketValue);
+			portfolioValue += parseFloat(quote.MarketValue);
 			dayPL += parseFloat(quote.todayPL);
 		});
 		dayPercent = dayPL / (portfolioValue - dayPL) * 100;
+		dayPLClass = dayPL >= 0 ? 'positive' : 'negative';
+		this.setSidebarColor(dayPLClass);
+		portfolioValue = this.numberFormat(portfolioValue, 2);
+		portfolioValue = portfolioValue.toString().split('.').join('<span class="market-value__cents">.');
+		portfolioValue += '</span>';
 		$('article').html(this.template({ 
 			quotes: this.quotes, 
-			portfolioValue: this.numberFormat(portfolioValue), 
-			dayPL: this.numberFormat(dayPL, 2),
-			dayPercent: this.numberFormat(dayPercent, 2)
+			portfolioValue: portfolioValue, 
+			dayPL: this.numberFormat(dayPL, 2, true),
+			dayPercent: this.numberFormat(dayPercent, 2),
+			dayPLClass: dayPLClass
 		}));
+		this.setBackgroundColor();
+	},
+	setSidebarColor: function(dayPLClass) {
+		var color = dayPLClass == 'positive' ? '#21CE99' : '#F9523A'
+		$('.logo').css('border', '3px solid ' + color);
+		$('.btn-logout').css('background', color)
 	}
 };
 $(document).ready(function() {
