bd7dcc06df42a64897e2a35917cf06d8a370ceea
Albert Tseng <atseng3@gmail.com>
2015-07-09 00:15:17 -0700

    complete job script to update market value everyday at 1310 pst

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -1,10 +1,60 @@
+Parse.Cloud.job('calculateMarketValue', function(request, status) {
+	var UserPortfolios = Parse.Object.extend('UserPortfolios');
+	var userPortfolio = new UserPortfolios;
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
+	// hard coded portfolioId
+	userPortfolio.set('portfolioId', 1);
+	userPortfolio.set('date', new Date());
 
-Parse.Cloud.job('marketValue', function() {
-	
+	// get portfolio stocks
+	var Portfolio = Parse.Object.extend('Portfolio');
+	var query = new Parse.Query(Portfolio);
+	query.exists('shares');
+	query.find({
+		success: function(quotes) {
+			var symbols = [];
+			var portfolio = {};
+			for(var i = 0; i < quotes.length; i++) {
+				var quote = quotes[i];
+				portfolio[quote.get('symbol')] = {
+					shares: quote.get('shares'),
+					cost: quote.get('cost')
+				};
+			}
+			symbols = Object.keys(portfolio);
+			var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(';
+			url += "%22" + symbols.join("%22%2C%22") + "%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+			Parse.Cloud.httpRequest({
+			  url: url,
+			  headers: {
+			    'Content-Type': 'application/json;charset=utf-8'
+			  }
+			}).then(function(httpResponse) {
+				var data = httpResponse.data;
+				var quotes = [];
+				
+	    	var raw_quotes = data.query.results.quote;
+
+	    	for(var j = 0; j < raw_quotes.length; j++) {
+	    		var quote = raw_quotes[j];
+	    		quotes.push({ symbol: quote.symbol, price: quote.LastTradePriceOnly });
+	    	}
+
+	    	var marketValue = 0;
+
+	    	for(var k = 0; k < quotes.length; k++) {
+	    		marketValue += portfolio[quotes[k].symbol]['shares'] * quotes[k].price;
+	    	}
+	    	marketValue = parseFloat(marketValue.toFixed(2));
+	    	userPortfolio.set('marketValue', marketValue);
+	    	userPortfolio.save(null, {
+					success: function(portfolio) {
+						status.success('Market Value is: '+marketValue);
+					}
+				});
+			}, function(httpResponse) {
+				status.error('Error is ' + httpResponse.status);
+			});
+		}
+	});
 });
